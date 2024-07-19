acde5f9cc9ea02ec35fe78e7c8e02ab5cdf4ae76
kumayoster <140373296+kumayoster@users.noreply.github.com>
2024-07-19 13:31:30 +0700

    kelupaan

--- 11PPLG1_Hansen/hansen_xipplg1.sql
+++ 11PPLG1_Hansen/hansen_xipplg1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2024 at 12:26 PM
+-- Generation Time: Jul 19, 2024 at 08:30 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,47 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `dokter`
+--
+
+CREATE TABLE `dokter` (
+  `idDokter` int(11) NOT NULL,
+  `nmDokter` varchar(100) DEFAULT NULL,
+  `spesialisasi` varchar(100) DEFAULT NULL,
+  `noTelp` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `dokter`
+--
+
+INSERT INTO `dokter` (`idDokter`, `nmDokter`, `spesialisasi`, `noTelp`) VALUES
+(12121, NULL, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kunjungan`
+--
+
+CREATE TABLE `kunjungan` (
+  `idKunjungan` int(11) NOT NULL,
+  `idPasien` varchar(10) NOT NULL,
+  `idDokter` int(11) NOT NULL,
+  `tanggal` date DEFAULT NULL,
+  `keluhan` text DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `kunjungan`
+--
+
+INSERT INTO `kunjungan` (`idKunjungan`, `idPasien`, `idDokter`, `tanggal`, `keluhan`) VALUES
+(12323, 'dsds', 12121, '2024-07-12', ' sdadsad');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pasien`
 --
 
@@ -35,14 +76,51 @@ CREATE TABLE `pasien` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `pasien`
+--
+
+INSERT INTO `pasien` (`idPasien`, `nmPasien`, `jk`, `alamat`) VALUES
+('dsds', '', '', '');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `dokter`
+--
+ALTER TABLE `dokter`
+  ADD PRIMARY KEY (`idDokter`);
+
+--
+-- Indexes for table `kunjungan`
+--
+ALTER TABLE `kunjungan`
+  ADD PRIMARY KEY (`idKunjungan`),
+  ADD UNIQUE KEY `idPasien` (`idPasien`),
+  ADD UNIQUE KEY `idDokter` (`idDokter`);
 
 --
 -- Indexes for table `pasien`
 --
 ALTER TABLE `pasien`
   ADD PRIMARY KEY (`idPasien`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `dokter`
+--
+ALTER TABLE `dokter`
+  MODIFY `idDokter` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12213;
+
+--
+-- AUTO_INCREMENT for table `kunjungan`
+--
+ALTER TABLE `kunjungan`
+  MODIFY `idKunjungan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12324;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
